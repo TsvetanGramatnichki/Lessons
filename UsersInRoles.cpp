05acf6b7a49fdb69e0c5f536995fe3ec74d3cc9a
TsvetanGramatnichki <gramatnichki@gmail.com>
2014-12-23 02:17:45 +0200

    UsersInRoles program
    Program with array and for loop, which prints UserID and RoleID.

---
+++ UsersInRoles.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class UsersInRoles
+{
+    int UserId;
+    int RoleId;
+
+public:
+    UsersInRoles(int iduser, int idrole);
+    UsersInRoles (int iduser);
+    UsersInRoles();
+
+    int getIdUser();
+    void setIdUser(int iduser);
+    int getIdRole();
+    void setIdRole(int idrole);
+
+    int Print()
+    {
+
+        cout<<"UserId=" <<UserId<<endl;
+        cout<<"RoleId=" <<RoleId<<endl;
+        return UserId;
+    }
+
+};
+
+UsersInRoles::UsersInRoles(int iduser, int idrole)
+{
+    UserId = iduser;
+    RoleId = idrole;
+}
+
+UsersInRoles::UsersInRoles(int iduser)
+{
+    UserId = iduser;
+}
+
+UsersInRoles::UsersInRoles()
+{
+
+}
+
+int UsersInRoles::getIdUser()
+{
+    return UserId;
+}
+
+void UsersInRoles::setIdUser(int iduser)
+{
+    UserId = iduser;
+}
+
+int UsersInRoles::getIdRole()
+{
+    return RoleId;
+}
+
+void UsersInRoles::setIdRole(int idrole)
+{
+    RoleId = idrole;
+}
+
+
+int main()
+{
+    int userid;
+    int roleid;
+    UsersInRoles Array[5];
+    int i;
+    for(i=0; i<5; i++)
+    {
+
+        cout<<"Enter UserID:\n";
+        cin>>userid;
+        cout<<"Enter RoleId:\n";
+        cin>>roleid;
+        cout<<endl;
+        UsersInRoles object(userid,roleid);
+        Array[i] = object;
+    }
+    cout<<"Information for the users:\n";
+    for(int i=0; i<5; i++)
+    {
+        cout<<Array[i].Print()<<endl;
+        cout<<endl;
+    }
+
+    return 0;
+}
